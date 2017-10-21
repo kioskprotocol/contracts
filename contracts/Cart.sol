f4a20135d780c1a605ac9245c74c3e483b03302e
richmcateer <rmcateer1@gmail.com>
2017-10-21 19:44:54 -0400

    Simplify cart

--- contracts/Cart.sol
+++ contracts/Cart.sol
@@ -4,18 +4,8 @@ pragma solidity ^0.4.11;
 // TODO: A decentralized application should be able to add items on behalf of the buyer.
 contract Cart {
     event AddToCart(uint256 indexed DIN, address indexed buyer);
-    event RemoveFromCart(uint256 indexed DIN, address indexed buyer);
-    event EmptyCart(address indexed buyer);
 
     function addToCart(uint256 DIN) {
         AddToCart(DIN, msg.sender);
-    }
-
-    function removeFromCart(uint256 DIN) {
-        RemoveFromCart(DIN, msg.sender);
-    }
-
-    function emptyCart() {
-        EmptyCart(msg.sender);
     }
 }
