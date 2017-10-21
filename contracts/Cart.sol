d8b3d0c55a4ffa29fbaa9eea6da9e8ce780ae83e
richmcateer <rmcateer1@gmail.com>
2017-10-21 19:46:07 -0400

    Update Cart.sol

--- contracts/Cart.sol
+++ contracts/Cart.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.11;
 
 /** @title Universal shopping cart */
-// TODO: A decentralized application should be able to add items on behalf of the buyer.
 contract Cart {
     event AddToCart(uint256 indexed DIN, address indexed buyer);
 
