b446b9315b8f929d8d2fadb4ee69b0d36ad19900
richmcateer <rmcateer1@gmail.com>
2017-10-27 16:29:32 -0400

    Avoid naming conflicts in standard resolver

--- contracts/StandardResolver.sol
+++ contracts/StandardResolver.sol
@@ -10,8 +10,8 @@ contract StandardResolver is Resolver {
     bytes4 constant MERCHANT_INTERFACE_ID = 0x90a9f8b8;     // bytes4(keccak256("merchant(uint256)"))
 
     DINRegistry public registry;
-    string public productURL;
-    address public merchant;
+    string productURL;
+    address merchant;
     address public owner;
 
     modifier only_owner {
