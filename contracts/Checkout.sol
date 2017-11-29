4fedff309d8d7ed98b5cdf0234af3a983e94ee23
richmcateer <rmcateer1@gmail.com>
2017-11-28 23:52:28 -0500

    Internal check on Checkout to allow struct passing

--- contracts/Checkout.sol
+++ contracts/Checkout.sol
@@ -30,9 +30,6 @@ contract Checkout {
         address loyaltyToken;
         address merchant;
         address owner;
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
     }
 
     // Log Solidity errors
@@ -110,29 +107,10 @@ contract Checkout {
             loyaltyReward: orderValues[5],
             loyaltyToken: orderAddresses[1],
             merchant: merchant,
-            owner: registry.owner(orderValues[0]), // Get the DIN owner address from the DIN registry.
-            v: v,
-            r: r,
-            s: s
+            owner: registry.owner(orderValues[0]) // Get the DIN owner address from the DIN registry.
         });
 
-        bool isValid = isValidOrder(
-            order.DIN,
-            order.quantity,
-            order.totalPrice,
-            order.priceValidUntil,
-            order.affiliateReward,
-            order.affiliate,
-            order.loyaltyReward,
-            order.loyaltyToken,
-            order.merchant,
-            order.owner,
-            order.v,
-            order.r,
-            order.s
-        );
-
-        if (isValid == false) {
+        if (isValidOrder(order, v, r, s) == false) {
             // Return Ether to buyer.
             msg.sender.transfer(msg.value);
             return 0;
@@ -189,64 +167,46 @@ contract Checkout {
       * @dev Verify that an order is valid.
       * @return valid Validity of the order.
       */
-    function isValidOrder(
-        uint256 DIN,
-        uint256 quantity,
-        uint256 totalPrice,
-        uint256 priceValidUntil,
-        uint256 affiliateReward,
-        address affiliate,
-        uint256 loyaltyReward,
-        address loyaltyToken,
-        address merchant,
-        address owner,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) 
-        public 
-        constant 
-        returns (bool) 
-    {
-        if (block.timestamp > priceValidUntil) {
+    function isValidOrder(Order order, uint8 v, bytes32 r, bytes32 s) internal constant returns (bool) {
+        if (block.timestamp > order.priceValidUntil) {
             LogError("Offer expired");
             return false;
         }
 
-        if (merchant == address(0x0)) {
+        if (order.merchant == address(0x0)) {
             LogError("Invalid merchant");
             return false;
         }
 
-        if (affiliateReward > 0 && affiliate == msg.sender) {
+        if (order.affiliateReward > 0 && order.affiliate == msg.sender) {
             LogError("Invalid affiliate");
             return false;
         }
 
-        if (loyaltyReward > 0 && loyaltyToken != address(0x0) && loyaltyRegistry.whitelist(loyaltyToken) == false) {
+        if (order.loyaltyReward > 0 && order.loyaltyToken != address(0x0) && loyaltyRegistry.whitelist(order.loyaltyToken) == false) {
             LogError("Invalid loyalty token");
             return false;
         }
 
-        if (msg.value > totalPrice) {
+        if (msg.value > order.totalPrice) {
             LogError("Invalid price");
             return false;
         }
 
-        uint256 unitPrice = totalPrice / quantity;
+        uint256 unitPrice = order.totalPrice / order.quantity;
 
         // Calculate the hash of the parameters provided by the buyer.
         bytes32 hash = keccak256(
-            DIN,
+            order.DIN,
             unitPrice,
-            priceValidUntil,
-            affiliateReward, 
-            loyaltyReward,
-            loyaltyToken
+            order.priceValidUntil,
+            order.affiliateReward, 
+            order.loyaltyReward,
+            order.loyaltyToken
         );
 
         // Verify that the DIN owner has signed the provided inputs.
-        if (isValidSignature(owner, hash, v, r, s) == false) {
+        if (isValidSignature(order.owner, hash, v, r, s) == false) {
             LogError("Invalid signature");
             return false;
         }
